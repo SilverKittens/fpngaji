b3ec0c80c44ee8b20ddae649b36d4806630028bc
satryabgs <bagusatrya1901@gmail.com>
2018-04-18 22:45:39 +0700

    first commit
    18 April 2018

---
+++ application/views/masuk.php
@@ -0,0 +1,115 @@
+<!DOCTYPE html>
+<html lang="en">
+
+  <head>
+
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="">
+    <meta name="author" content="">
+
+    <title>NgajiKuy</title>
+
+    <!-- Bootstrap core CSS -->
+    <link href="<?php echo base_url('asset/vendor/bootstrap/css/bootstrap.min.css')?>" rel="stylesheet">
+
+    <!-- Custom fonts for this template -->
+    <link rel="stylesheet" href="<?php echo base_url('asset/vendor/font-awesome/css/font-awesome.min.css')?>">
+    <link rel="stylesheet" href="<?php echo base_url('asset/vendor/simple-line-icons/css/simple-line-icons.css')?>">
+    <link href="<?php echo base_url('https://fonts.googleapis.com/css?family=Lato')?>" rel="stylesheet">
+    <link href="<?php echo base_url('https://fonts.googleapis.com/css?family=Catamaran:100,200,300,400,500,600,700,800,900')?>" rel="stylesheet">
+    <link href="<?php echo base_url('https://fonts.googleapis.com/css?family=Muli')?>" rel="stylesheet">
+    <style>
+
+    ::placeholder { /* Chrome, Firefox, Opera, Safari 10.1+ */
+        color: white;
+        opacity: 1; /* Firefox */
+    }
+    </style>
+
+    <!-- Custom styles for this template -->
+    <link href="<?php echo base_url('asset/css/new-age.min.css')?>" rel="stylesheet">
+
+  </head>
+
+  <body id="page-top">
+
+    <!-- Navigation -->
+    <nav class="navbar navbar-expand-lg navbar-light fixed-top" id="mainNav">
+      <div class="container">
+        <a class="navbar-brand js-scroll-trigger" href="<?php echo base_url('home') ?>">Halaman Utama</a>
+        <button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
+          Menu
+          <i class="fa fa-bars"></i>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarResponsive">
+          <ul class="navbar-nav ml-auto">
+            <li class="nav-item">
+              <a class="nav-link js-scroll-trigger" href="">Masuk</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link js-scroll-trigger" href="<?php echo base_url('daftarakun') ?>">Daftar</a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+
+    <header class="masthead" style="background: url('asset/img/bg-pattern.png'), linear-gradient(to left, #df7000,#5fb000)">
+      <div class="container h-100">
+        <div class="row h-100">
+          <div class="col-lg-7 my-auto">
+            <div class="header-content mx-auto" style="text-align:center">
+              <h1 class="mb-5">Ayo ngaji sekarang!</h1>
+             <img src="<?php echo base_url('asset/img/bo1.png') ?>" style="width:40%;" alt="">
+
+            </div>
+          </div>
+          <div class="col-lg-5 my-auto">
+            <form class="" action="" method="post">
+              <h5>E-mail :</h5> <input required type="email" name="" placeholder="E-mail" style="border-radius: 7px;background: transparent; border-color:white;color:white;padding:15px;width:100%"><br><br>
+              <h5>Password :</h5> <input required type="password" name="" placeholder="password" style="border-radius: 7px;background: transparent; border-color:white;color:white;padding:15px;width:100%"><br><br>
+            </form>
+            <br><br>
+              <a href="" class="btn btn-outline btn-xl js-scroll-trigger">Masuk</a>
+                    <!-- Demo image for screen mockup, you can put an image here, some HTML, an animation, video, or anything else! -->
+
+                  <div class="button">
+                    <!-- You can hook the "home button" to some JavaScript events or just remove it -->
+                  </div>
+
+          </div>
+        </div>
+      </div>
+    </header>
+
+    <footer>
+      <div class="container">
+        <p>&copy; Your Website 2018. All Rights Reserved.</p>
+        <ul class="list-inline">
+          <li class="list-inline-item">
+            <a href="#">Privacy</a>
+          </li>
+          <li class="list-inline-item">
+            <a href="#">Terms</a>
+          </li>
+          <li class="list-inline-item">
+            <a href="#">FAQ</a>
+          </li>
+        </ul>
+      </div>
+    </footer>
+
+    <!-- Bootstrap core JavaScript -->
+    <script src="<?php echo base_url('asset/vendor/jquery/jquery.min.js')?>"></script>
+    <script src="<?php echo base_url('asset/vendor/bootstrap/js/bootstrap.bundle.min.js')?>"></script>
+
+    <!-- Plugin JavaScript -->
+    <script src="<?php echo base_url('asset/vendor/jquery-easing/jquery.easing.min.js')?>"></script>
+
+    <!-- Custom scripts for this template -->
+    <script src="<?php echo base_url('asset/js/new-age.min.js')?>"></script>
+
+  </body>
+
+</html>
---
+++ application/views/welcome_message.php
@@ -0,0 +1,206 @@
+<!DOCTYPE html>
+<html lang="en">
+
+  <head>
+
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="">
+    <meta name="author" content="">
+
+    <title>NgajiKuy</title>
+
+    <!-- Bootstrap core CSS -->
+    <link href="<?php echo base_url('asset/vendor/bootstrap/css/bootstrap.min.css')?>" rel="stylesheet">
+
+    <!-- Custom fonts for this template -->
+    <link rel="stylesheet" href="<?php echo base_url('asset/vendor/font-awesome/css/font-awesome.min.css')?>">
+    <link rel="stylesheet" href="<?php echo base_url('asset/vendor/simple-line-icons/css/simple-line-icons.css')?>">
+    <link href="<?php echo base_url('https://fonts.googleapis.com/css?family=Lato')?>" rel="stylesheet">
+    <link href="<?php echo base_url('https://fonts.googleapis.com/css?family=Catamaran:100,200,300,400,500,600,700,800,900')?>" rel="stylesheet">
+    <link href="<?php echo base_url('https://fonts.googleapis.com/css?family=Muli')?>" rel="stylesheet">
+
+
+    <!-- Custom styles for this template -->
+    <link href="<?php echo base_url('asset/css/new-age.min.css')?>" rel="stylesheet">
+
+  </head>
+
+  <body id="page-top">
+
+    <!-- Navigation -->
+    <nav class="navbar navbar-expand-lg navbar-light fixed-top" id="mainNav">
+      <div class="container">
+        <a class="navbar-brand js-scroll-trigger" href="#page-top">Halaman Utama</a>
+        <button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
+          Menu
+          <i class="fa fa-bars"></i>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarResponsive">
+          <ul class="navbar-nav ml-auto">
+            <li class="nav-item">
+              <a class="nav-link js-scroll-trigger" href="#features">Fasilitas</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link js-scroll-trigger" href="#cta">Buat Akun</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link js-scroll-trigger" href="#contact">Hubungi Kami</a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+
+    <header class="masthead">
+      <div class="container h-100">
+        <div class="row h-100">
+          <div class="col-lg-7 my-auto">
+            <div class="header-content mx-auto">
+              <h1 class="mb-5">Ngaji Kuy adalah tempat bla bla bla untuk belajar mengaji secara online. Tidak perlu repot untuk harus mencari guru ngaji!</h1>
+              <a href="#download" class="btn btn-outline btn-xl js-scroll-trigger">Daftar Sekarang!</a>
+            </div>
+          </div>
+          <div class="col-lg-5 my-auto">
+                    <!-- Demo image for screen mockup, you can put an image here, some HTML, an animation, video, or anything else! -->
+                    <img src="<?php echo base_url('asset/img/boy.png')?>" class="img-fluid" alt="" style="width:65%">
+
+                  <div class="button">
+                    <!-- You can hook the "home button" to some JavaScript events or just remove it -->
+                  </div>
+
+          </div>
+        </div>
+      </div>
+    </header>
+
+    <section class="download bg-primary text-center" id="download">
+      <div class="container">
+        <div class="row">
+          <div class="col-md-8 mx-auto">
+            <img src="<?php echo base_url('asset/img/1hadis.png')?>" alt="" width="100%" style="margin-bottom:40px">
+
+            <p>“Abdullah bin Mas’ud radhiyallahu ‘anhu berkata: “Rasulullah shallallahu ‘alaihi wasallam bersabda: “Siapa yang membaca satu huruf dari Al Quran maka baginya satu kebaikan dengan bacaan tersebut, satu kebaikan dilipatkan menjadi 10 kebaikan semisalnya dan aku tidak mengatakan الم satu huruf akan tetapi Alif satu huruf, Laam satu huruf dan Miim satu huruf.”</p>
+          </div>
+        </div>
+      </div>
+    </section>
+
+    <section class="features" id="features">
+      <div class="container">
+        <div class="section-heading text-center">
+          <h2>Segera Daftar Sekarang!</h2>
+          <p class="text-muted">Belajar ngaji sekarang ga perlu sendirian!</p>
+          <hr>
+        </div>
+        <div class="row">
+          <div class="col-lg-4 my-auto">
+            <div class="device-container">
+              <img src="<?php echo base_url('asset/img/girl.png')?>"alt="" width="80%">
+            </div>
+          </div>
+          <div class="col-lg-8 my-auto">
+            <div class="container-fluid">
+              <div class="row">
+                <div class="col-lg-6">
+                  <div class="feature-item">
+                    <img src="<?php echo base_url('asset/img/laptop.png')?>" width="30%" style="margin-bottom:20px"></i>
+                    <h3>Video Call</h3>
+                    <p class="text-muted">Belajar online menggunakan video call secara real time!</p>
+                  </div>
+                </div>
+                <div class="col-lg-6">
+                  <div class="feature-item">
+                    <img src="<?php echo base_url('asset/img/orang.png')?>" width="30%" style="margin-bottom:20px"></i>
+                    <h3>Pengajar yang ramah</h3>
+                    <p class="text-muted">Ustad dan Ustadzah yang ramah serta berpengalaman dalam mengajar!</p>
+                  </div>
+                </div>
+              </div>
+              <div class="row">
+                <div class="col-lg-6">
+                  <div class="feature-item">
+                    <img src="<?php echo base_url('asset/img/dompet.png')?>" width="30%" style="margin-bottom:20px"></i>
+                    <h3>Harga Terjangkau</h3>
+                    <p class="text-muted">Harga yang kami tawarkan merupakan harga yang bagus!</p>
+                  </div>
+                </div>
+                <div class="col-lg-6">
+                  <div class="feature-item">
+                    <img src="<?php echo base_url('asset/img/piala.png')?>" width="25%" height="90%" style="margin-bottom:20px"></i>
+                    <h3>Memiliki Sertifikat</h3>
+                    <p class="text-muted">Ustad dan Ustadzah di NgajiKuy memiliki sertifikat </p>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+
+    <section class="cta" id="cta">
+      <div class="cta-content">
+        <div class="container">
+          <h2>Stop waiting.<br>Start Reading.</h2>
+          <a href="<?php echo base_url('daftarakun') ?>" class="btn btn-outline btn-xl js-scroll-trigger">Let's Get Started!</a>
+        </div>
+      </div>
+      <div class="overlay"></div>
+    </section>
+
+    <section class="contact bg-primary" id="contact">
+      <div class="container">
+        <h2>Kami
+          <i class="fa fa-heart"></i>
+          Al - Quran!</h2>
+        <ul class="list-inline list-social">
+          <li class="list-inline-item social-twitter">
+            <a href="#">
+              <i class="fa fa-twitter"></i>
+            </a>
+          </li>
+          <li class="list-inline-item social-facebook">
+            <a href="#">
+              <i class="fa fa-facebook"></i>
+            </a>
+          </li>
+          <li class="list-inline-item social-google-plus">
+            <a href="#">
+              <i class="fa fa-google-plus"></i>
+            </a>
+          </li>
+        </ul>
+      </div>
+    </section>
+
+    <footer>
+      <div class="container">
+        <p>&copy; Your Website 2018. All Rights Reserved.</p>
+        <ul class="list-inline">
+          <li class="list-inline-item">
+            <a href="#">Privacy</a>
+          </li>
+          <li class="list-inline-item">
+            <a href="#">Terms</a>
+          </li>
+          <li class="list-inline-item">
+            <a href="#">FAQ</a>
+          </li>
+        </ul>
+      </div>
+    </footer>
+
+    <!-- Bootstrap core JavaScript -->
+    <script src="<?php echo base_url('asset/vendor/jquery/jquery.min.js')?>"></script>
+    <script src="<?php echo base_url('asset/vendor/bootstrap/js/bootstrap.bundle.min.js')?>"></script>
+
+    <!-- Plugin JavaScript -->
+    <script src="<?php echo base_url('asset/vendor/jquery-easing/jquery.easing.min.js')?>"></script>
+
+    <!-- Custom scripts for this template -->
+    <script src="<?php echo base_url('asset/js/new-age.min.js')?>"></script>
+    <script type='text/javascript' data-cfasync='false'>window.purechatApi = { l: [], t: [], on: function () { this.l.push(arguments); } }; (function () { var done = false; var script = document.createElement('script'); script.async = true; script.type = 'text/javascript'; script.src = 'https://app.purechat.com/VisitorWidget/WidgetScript'; document.getElementsByTagName('HEAD').item(0).appendChild(script); script.onreadystatechange = script.onload = function (e) { if (!done && (!this.readyState || this.readyState == 'loaded' || this.readyState == 'complete')) { var w = new PCWidget({c: 'a2481ef2-e29b-43d1-b827-7b9b2b0c3033', f: true }); done = true; } }; })();</script>
+  </body>
+
+</html>
